88b8283960f12648931e62a9bb8c6847f8100721
Chetan Dwarkani <mchetandwarkani@gmail.com>
2019-10-12 09:30:37 -0700

    first screen and basic inits

--- app/src/main/java/com/dwarsoftgames/dwarsoft_lynk_hackathon/MainActivity.java
+++ app/src/main/java/com/dwarsoftgames/dwarsoft_lynk_hackathon/MainActivity.java
@@ -2,7 +2,10 @@ package com.dwarsoftgames.dwarsoft_lynk_hackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+    }
+
+    public void signup(View view) {
+        startActivity(new Intent(this, SignUp.class));
     }
 }
